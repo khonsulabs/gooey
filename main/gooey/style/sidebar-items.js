9324b0ea76f19b53cbedf0f0dd1d39cd9ea57ffa
kl-botsu <botsu@khonsulabs.com>
2021-08-17 21:20:10 +0000

    Deploying to gh-pages from @ khonsulabs/gooey@9d7f2ea51021ac3c9fa2dc56a067b58883a8bf50 🚀

--- main/gooey/style/sidebar-items.js
+++ main/gooey/style/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["default_stylesheet","The default [`StyleSheet`] for `Gooey`."]]});
+initSidebarItems({"fn":[["default_stylesheet","The default [`StyleSheet`] for `Gooey`."],["stylesheet_for_palette","Creates a stylesheet using the [`Palette`] provided."]],"trait":[["Palette","Returns a set of colors that form a palette."]]});
